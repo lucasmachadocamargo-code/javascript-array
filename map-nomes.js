020c0e3d94864e199d008174777ce330cc9dbd02
lucasmachadocamargo-code <lucas.machado.camargo@escola.pr.gov.br>
2026-08-06 14:43:10 +0000

    .ç.....

---
+++ map-nomes.js
@@ -0,0 +1,7 @@
+const nomes = ["lucas wendel " , "maycon ","luan "];
+
+const nomesplanorizado = nomes.map (( nome)=>{
+return nome.toUperCase();
+});
+
+console.log( nomesplanorizado);
--- map.js
+++ map.js
@@ -1,5 +1,8 @@
 const notas = [10, 9.5, 8,7,6 ];
 
-notas.map(()=>{
 
+const notasAtualizadas = notas.map((nota)=>{
+return nota + 1 >= 10 ? 10 : nota + 1;
 });
+
+console.log( notasAtualizadas);
